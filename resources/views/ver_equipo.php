ecf8f2fa48a10c4c8f01d8fdf090fd07b965b5cb
Frangonlop <frangonlop004@gmail.com>
2024-06-07 12:11:37 +0200

    show types in spanish

--- resources/views/ver_equipo.php
+++ resources/views/ver_equipo.php
@@ -72,9 +72,18 @@ foreach ($pokemons as $nombre_pokemon) {
     $url = "https://pokeapi.co/api/v2/pokemon/" . strtolower($nombre_pokemon);
     $pokemon_data = json_decode(file_get_contents($url), true);
     if ($pokemon_data) {
+        $tipo_url = $pokemon_data['types'][0]['type']['url'];
+        $tipo_data = json_decode(file_get_contents($tipo_url), true);
+        $tipo_es = '';
+        foreach ($tipo_data['names'] as $name) {
+            if ($name['language']['name'] == 'es') {
+                $tipo_es = $name['name'];
+                break;
+            }
+        }
         $pokemon_details[] = [
             'nombre' => $pokemon_data['name'],
-            'tipo' => $pokemon_data['types'][0]['type']['name'],
+            'tipo' => $tipo_es,
             'imagen' => $pokemon_data['sprites']['front_default']
         ];
     }
@@ -82,42 +91,49 @@ foreach ($pokemons as $nombre_pokemon) {
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Detalles del Equipo</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap"
+        rel="stylesheet">
     <link rel="stylesheet" href="../css/main.css">
 </head>
 <body>
-    <nav>
-        <ul class="menu">
-            <li><a href="../../index.php" class="teams">Inicio</a></li>
-            <?php if (isset($_SESSION['login'])): ?>
-                <li><a class="name signup"><?php echo htmlspecialchars($_SESSION['login']); ?></a></li>
-                <li><a href="../model/logout.php" class="logout">Cerrar sesión</a></li>
-            <?php else: ?>
-                <li><a href="login.php?redirect=teams.php">Iniciar sesión</a></li>
-            <?php endif; ?>
-        </ul>
-    </nav>
-    <h1>Detalles del Equipo</h1>
-    <h2><?php echo htmlspecialchars($nombre_equipo); ?></h2>
-    <p>Fecha de creación: <?php echo htmlspecialchars($fecha_creacion); ?></p>
-    
-    <h3>Pokémon en el equipo</h3>
-    <?php if (empty($pokemon_details)): ?>
-        <p>No hay Pokémon en este equipo.</p>
-    <?php else: ?>
-        <ul>
-            <?php foreach ($pokemon_details as $pokemon): ?>
-                <li>
-                    <img src="<?php echo htmlspecialchars($pokemon['imagen']); ?>" alt="<?php echo htmlspecialchars($pokemon['nombre']); ?>">
-                    <?php echo htmlspecialchars($pokemon['nombre']); ?> - <?php echo htmlspecialchars($pokemon['tipo']); ?>
-                </li>
-            <?php endforeach; ?>
-        </ul>
-    <?php endif; ?>
-    <p><a href="teams.php">Volver a mis equipos</a></p>
+    <div class="container">
+        <nav>
+            <ul class="menu">
+                <li><a href="../../index.php" class="teams">Inicio</a></li>
+                <?php if (isset($_SESSION['login'])): ?>
+                    <li><a class="name signup"><?php echo htmlspecialchars($_SESSION['login']); ?></a></li>
+                    <li><a href="../model/logout.php" class="logout">Cerrar sesión</a></li>
+                <?php else: ?>
+                    <li><a href="login.php?redirect=teams.php">Iniciar sesión</a></li>
+                <?php endif; ?>
+            </ul>
+        </nav>
+        <p><a href="teams.php">Volver a mis equipos</a></p>
+        <h1>Detalles del Equipo</h1>
+        <h2><?php echo htmlspecialchars($nombre_equipo); ?></h2>
+        <p>Fecha de creación: <?php echo htmlspecialchars($fecha_creacion); ?></p>
+        
+        <h3>Pokémon en el equipo</h3>
+        <?php if (empty($pokemon_details)): ?>
+            <p>No hay Pokémon en este equipo.</p>
+        <?php else: ?>
+            <ul>
+                <?php foreach ($pokemon_details as $pokemon): ?>
+                    <li>
+                        <img src="<?php echo htmlspecialchars($pokemon['imagen']); ?>" alt="<?php echo htmlspecialchars($pokemon['nombre']); ?>">
+                        <?php echo htmlspecialchars($pokemon['nombre']); ?> - <?php echo htmlspecialchars($pokemon['tipo']); ?>
+                    </li>
+                <?php endforeach; ?>
+            </ul>
+        <?php endif; ?>
+    </div>
 </body>
 </html>
